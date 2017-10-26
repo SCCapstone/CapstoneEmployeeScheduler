4bf0f8f013347c47156d74cdc6f2c89c999a813d
Chance Reynolds <chanc@DESKTOP-06688T3>
2017-10-26 12:58:50 -0400

    Merge

--- m3-chancetrschool/m3-chancetrschool/MainWindow.xaml.cs
+++ m3-chancetrschool/m3-chancetrschool/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 
-namespace CapstoneEmployeeScheduler
+namespace m3_chancetrschool
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
