67531fc10d310eb7dce708728a5f740fee4aa3c6
Chance Reynolds <chancetr@email.sc.edu>
2017-11-07 15:52:08 -0500

    Made the User and Role Models.

--- CapstoneEmployeeScheduler/Models/Role.cs
+++ CapstoneEmployeeScheduler/Models/Role.cs
@@ -8,5 +8,33 @@ namespace CapstoneEmployeeScheduler.Model
 {
     class Role
     {
+        private int ID;
+        private string roleName;
+
+        public int ID1
+        {
+            get
+            {
+                return ID;
+            }
+
+            set
+            {
+                ID = value;
+            }
+        }
+
+        public string RoleName
+        {
+            get
+            {
+                return roleName;
+            }
+
+            set
+            {
+                roleName = value;
+            }
+        }
     }
 }
--- CapstoneEmployeeScheduler/Models/User.cs
+++ CapstoneEmployeeScheduler/Models/User.cs
@@ -8,5 +8,159 @@ namespace CapstoneEmployeeScheduler.Model
 {
     class User
     {
+        private int ID;
+        private string userName;
+        private string email;
+        private string shift;
+        private int roleOneDayAgo;
+        private int roleTwoDaysAgo;
+        private int roleThreeDaysAgo;
+        private bool outOfWork;
+        private bool disabled;
+        private bool admin;
+        private List<Role> roles;
+
+        public int ID1
+        {
+            get
+            {
+                return ID;
+            }
+
+            set
+            {
+                ID = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+
+            set
+            {
+                userName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Shift
+        {
+            get
+            {
+                return shift;
+            }
+
+            set
+            {
+                shift = value;
+            }
+        }
+
+        public int RoleOneDayAgo
+        {
+            get
+            {
+                return roleOneDayAgo;
+            }
+
+            set
+            {
+                roleOneDayAgo = value;
+            }
+        }
+
+        public int RoleTwoDaysAgo
+        {
+            get
+            {
+                return roleTwoDaysAgo;
+            }
+
+            set
+            {
+                roleTwoDaysAgo = value;
+            }
+        }
+
+        public int RoleThreeDaysAgo
+        {
+            get
+            {
+                return roleThreeDaysAgo;
+            }
+
+            set
+            {
+                roleThreeDaysAgo = value;
+            }
+        }
+
+        public bool OutOfWork
+        {
+            get
+            {
+                return outOfWork;
+            }
+
+            set
+            {
+                outOfWork = value;
+            }
+        }
+
+        public bool Disabled
+        {
+            get
+            {
+                return disabled;
+            }
+
+            set
+            {
+                disabled = value;
+            }
+        }
+
+        public bool Admin
+        {
+            get
+            {
+                return admin;
+            }
+
+            set
+            {
+                admin = value;
+            }
+        }
+
+        internal List<Role> Roles
+        {
+            get
+            {
+                return roles;
+            }
+
+            set
+            {
+                roles = value;
+            }
+        }
     }
 }
