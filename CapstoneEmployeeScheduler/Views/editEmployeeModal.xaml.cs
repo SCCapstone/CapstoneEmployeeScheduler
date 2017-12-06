6ec125550177e824acead291559a75ba2a21ac1a
Chris Daigle <cdaigle@email.sc.edu>
2017-12-06 15:14:30 -0500

    Edit employee will edit; does not populate yet

---
+++ CapstoneEmployeeScheduler/Views/editEmployeeModal.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CapstoneEmployeeScheduler.Model;
+using CapstoneEmployeeScheduler.Controllers;
+namespace CapstoneEmployeeScheduler.Views
+{
+    /// <summary>
+    /// Interaction logic for editEmployeeModal.xaml
+    /// </summary>
+    public partial class editEmployeeModal : Window
+    {
+        public editEmployeeModal()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            UserController uc = new UserController();
+            User user = new User();
+            user.userName = name.Text;
+            user.email = email.Text;
+            uc.editUser(user);
+            
+        }
+
+        private void email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
