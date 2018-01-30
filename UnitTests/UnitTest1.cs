f345e5d7c5f0f87aad5cf7d72c40ea1dc5216e30
Chance Reynolds <chancetr@email.sc.edu>
2018-01-29 21:33:27 -0500

    Set up the unit tests.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapstoneEmployeeScheduler.Views;
+using CapstoneEmployeeScheduler;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {  
+        }
+    }
+}
