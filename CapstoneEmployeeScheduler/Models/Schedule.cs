6787cbb530c278b40c47b9faa32ce83f8c074b4e
Chance Reynolds <chancetr@email.sc.edu>
2018-02-16 23:21:16 -0500

    Setting up schedule back end.

---
+++ CapstoneEmployeeScheduler/Models/Schedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneEmployeeScheduler.Models
+{
+    class Schedule
+    {
+        public Dictionary<int, int> userRoles;
+
+        public Dictionary<int, int> UserRoles
+        {
+            get
+            {
+                return userRoles;
+            }
+
+            set
+            {
+                userRoles = value;
+            }
+        }
+    }
+}
