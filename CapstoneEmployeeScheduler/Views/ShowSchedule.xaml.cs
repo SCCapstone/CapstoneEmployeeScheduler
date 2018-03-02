95e49913153b994362cbde3201592953d0202cbe
THOMAS KENNER <tkenner@email.sc.edu>
2018-03-02 13:47:17 -0500

    Created Rows and Columns for the Display Schedule. Still doesnt work, DataGrid Errors.

--- CapstoneEmployeeScheduler/Views/ShowSchedule.xaml.cs
+++ CapstoneEmployeeScheduler/Views/ShowSchedule.xaml.cs
@@ -11,17 +11,52 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace CapstoneEmployeeScheduler.Views
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    /// 
+     
+    
+    
     public partial class ShowSchedule : Window
     {
+
+        DataGrid dt = new DataGrid();
+            int numberofEmployees = CapstoneEmployeeScheduler.Algorithm.makeSchedule.userCount;
+
         public ShowSchedule()
         {
+            //InitializeComponent();
             
+            AddCols();
+            AddRows();
+            WriteRows();
+            
+        }
+
+        private void AddCols()
+        {
+            DataGridTextColumn textColumn = new DataGridTextColumn();
+            textColumn.Header = "Employee";
+            textColumn.Binding = new System.Windows.Data.Binding("Employee");
+            dataGridView1.Columns.Add(textColumn);
+        }
+
+        private void AddRows()
+        {
+            for(int i =0; i < numberofEmployees; i++)
+            {
+                dt.Rows.Add();
+            }
+        }
+
+        private void WriteRows()
+        {
+           dataGridView1.ItemsSource = dt;
         }
     }
 }
