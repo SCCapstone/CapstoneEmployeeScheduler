84906da48a1d00f48be5518ba47b04c83920fbf0
ERIC FORMICA <eformica@email.sc.edu>
2018-03-04 17:50:24 -0500

    Moved CSV export to respective pages of application

--- CapstoneEmployeeScheduler/Views/Employees.xaml.cs
+++ CapstoneEmployeeScheduler/Views/Employees.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Shapes;
 using CapstoneEmployeeScheduler.Views;
 using CapstoneEmployeeScheduler.Model;
 using CapstoneEmployeeScheduler.Controllers;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
 namespace CapstoneEmployeeScheduler.Views
 {
     /// <summary>
@@ -85,6 +89,58 @@ namespace CapstoneEmployeeScheduler.Views
             }
         }
 
+        private void CSVEButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Employee Export
+            string connection = (string)System.Windows.Application.Current.FindResource("Connection");
+            string queryString = "SELECT * from Users;";
+            SqlDataAdapter adapter = new SqlDataAdapter(selectCommandText: queryString, selectConnectionString: connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, srcTable: "Users");
+            DataTable data = ds.Tables[0];
+            string path = @"C:\Users\Public\Documents\Users.csv";
+            CreateCSVFile(data, path);
+            System.Windows.MessageBox.Show("CSV File created. Please check your C:\\Users\\Public\\Documents.", "Created!");
+        }
+
+        void CreateCSVFile(DataTable dtDataTablesList, string strFilePath)
+        {
+            // Create the CSV file to which grid data will be exported.
+            StreamWriter sw = new StreamWriter(strFilePath, false);
+            sw.Write("sep = \t");
+            sw.Write(sw.NewLine);
+            //First we will write the headers.
+            int iColCount = dtDataTablesList.Columns.Count;
+            for (int i = 1; i < iColCount; i++)
+            {
+                sw.Write(dtDataTablesList.Columns[i]);
+                if (i < iColCount - 1)
+                {
+                    sw.Write(" ");
+                    sw.Write("\t");
+                }
+            }
+            sw.Write(sw.NewLine);
+
+            // Now write all the rows.
+            foreach (DataRow dr in dtDataTablesList.Rows)
+            {
+                for (int i = 1; i < iColCount; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        sw.Write(dr[i].ToString());
+                    }
+                    if (i < iColCount - 1)
+                    {
+                        sw.Write(" ");
+                        sw.Write("\t");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
     }
 
 
