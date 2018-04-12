56ec1b21629041a5f3ce1ac663b95487f41f676d
Franklin Piedade <piedade@email.sc.edu>
2018-04-12 18:38:34 -0400

    history edits

--- CapstoneEmployeeScheduler/Views/PastSchedule.xaml.cs
+++ CapstoneEmployeeScheduler/Views/PastSchedule.xaml.cs
@@ -45,5 +45,33 @@ namespace CapstoneEmployeeScheduler.Views
             showTheSchedule.ItemsSource = dt.DefaultView;
 
         }
+
+        public PastSchedule(DateTime aDate)
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            Schedule s = new Schedule();
+            ScheduleController sc = new ScheduleController();
+            UserController uc = new UserController();
+            RoleController rc = new RoleController();
+            s = sc.getScheduleByDate(aDate);
+
+            dt.Columns.Add("Employee", typeof(string));
+            dt.Columns.Add("Shift", typeof(string));
+            dt.Columns.Add("Role", typeof(string));
+
+            for (int i=0; i< s.UserRoles.Count; i++)
+            {
+                User u = uc.getUserById(s.UserRoles.ElementAt(i).Key);
+                Role r = rc.getRoleById(s.UserRoles.ElementAt(i).Value);
+                dt.Rows.Add(u.UserName, u.Shift, r.RoleName);
+            }
+            showTheSchedule.ItemsSource = dt.DefaultView;
+        }
+
+        private void showTheSchedule_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
