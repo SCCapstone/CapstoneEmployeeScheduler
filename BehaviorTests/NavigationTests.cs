9cd14bbe28b89d1d52b4776326091983e4b36a78
ERIC FORMICA <eformica@email.sc.edu>
2018-04-19 14:05:34 -0400

    Created 3 new Behavior Tests

--- BehaviorTests/NavigationTests.cs
+++ BehaviorTests/NavigationTests.cs
@@ -1,98 +1,97 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text.RegularExpressions;
-//using System.Windows.Input;
-//using System.Windows.Forms;
-//using System.Drawing;
-//using Microsoft.VisualStudio.TestTools.UITesting;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Microsoft.VisualStudio.TestTools.UITest.Extension;
-//using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using Microsoft.VisualStudio.TestTools.UITest.Common.UIMap;
 
+namespace BehaviorTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class NavigationTests
+    {
+        public NavigationTests()
+        {
+        }
 
-//namespace BehaviorTests
-//{
-//    /// <summary>
-//    /// Summary description for CodedUITest1
-//    /// </summary>
-//    [CodedUITest]
-//    public class CodedUITest1
-//    {
-//        public CodedUITest1()
-//        {
-//        }
+        [TestMethod]
+        public void AppWalkthrough()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.Walkthrough();
+        }
+        [TestMethod]
+        public void AddEmployee()
+        {
 
-//        [TestMethod]
-//        public void LoginTest()
-//        {
+            this.UIMap.AddEmployee();
 
-//            this.UIMap.LoginTest();
+        }
+        [TestMethod]
+        public void AddRole()
+        {
 
-//            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.CreateRole();
 
+        }
+        #region Additional test attributes
 
-//        }
+        // You can use the following additional attributes as you write your tests:
 
-//        [TestMethod]
-//        public void NavTest()
-//        {
-//            this.UIMap.NavigatingPages();
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
 
-//            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
 
+        #endregion
 
-//        }
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
 
-//        #region Additional test attributes
+        public UIMap UIMap
+        {
+            get
+            {
+                if (this.map == null)
+                {
+                    this.map = new UIMap();
+                }
 
-//        // You can use the following additional attributes as you write your tests:
+                return this.map;
+            }
+        }
 
-//        ////Use TestInitialize to run code before running each test 
-//        //[TestInitialize()]
-//        //public void MyTestInitialize()
-//        //{        
-//        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-//        //}
-
-//        ////Use TestCleanup to run code after each test has run
-//        //[TestCleanup()]
-//        //public void MyTestCleanup()
-//        //{        
-//        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-//        //}
-
-//        #endregion
-
-//        /// <summary>
-//        ///Gets or sets the test context which provides
-//        ///information about and functionality for the current test run.
-//        ///</summary>
-//        public TestContext TestContext
-//        {
-//            get
-//            {
-//                return testContextInstance;
-//            }
-//            set
-//            {
-//                testContextInstance = value;
-//            }
-//        }
-//        private TestContext testContextInstance;
-
-//        public UIMap UIMap
-//        {
-//            get
-//            {
-//                if (this.map == null)
-//                {
-//                    this.map = new UIMap();
-//                }
-
-//                return this.map;
-//            }
-//        }
-
-//        private UIMap map;
-//    }
-//}
+        private UIMap map;
+    }
+}
