b72c10aedd37c55193ef5a5f0cc56705c7195634
ERIC FORMICA <eformica@email.sc.edu>
2018-04-30 18:08:12 -0400

    Added comments to History.xaml.cs

--- CapstoneEmployeeScheduler/Views/History.xaml.cs
+++ CapstoneEmployeeScheduler/Views/History.xaml.cs
@@ -24,6 +24,8 @@ namespace CapstoneEmployeeScheduler.Views
 {
     /// <summary>
     /// Interaction logic for History.xaml
+    /// This page is used to display all schedules that have been created. This can be future, today, or past schedules created
+    /// The user can select any of the schedules and view them or delete them. When viewing them, they can also print the schedule
     /// </summary>
     public partial class History : Page
     {
@@ -39,7 +41,9 @@ namespace CapstoneEmployeeScheduler.Views
             List<Schedule> items = new List<Schedule>();
             ScheduleController sc = new ScheduleController();
             InitializeComponent();
+            //Creates a column of dates for the schedules to be displayed
             dt.Columns.Add("Date", typeof(DateTime));
+            //Gets the schedules for the next 6 days. If they are not blank, display them in the table
             Schedule s = sc.getScheduleByDate(DateTime.Today.AddDays(6));
             if (s.Id != null)
             {
@@ -75,6 +79,7 @@ namespace CapstoneEmployeeScheduler.Views
             {
                 dt.Rows.Add(DateTime.Today);
             }
+            //Gets the schedules for the past 6 days. If they are not blank, display them in the table
             s = sc.getScheduleByDate(DateTime.Today.AddDays(-1));
             if (s.Id != null)
             {
@@ -105,7 +110,7 @@ namespace CapstoneEmployeeScheduler.Views
             {
                 dt.Rows.Add(DateTime.Today.AddDays(-6));
             }
-
+            //Once we have all the schedules, display the full(or empty) table.
             GeneratedSchedules.SelectedIndex = 0;
             GeneratedSchedules.ItemsSource = dt.DefaultView;
         }
@@ -113,18 +118,22 @@ namespace CapstoneEmployeeScheduler.Views
         private void view_Click(object sender, RoutedEventArgs e)
         {
             //Method to display schedule when picked in the table
-            DataRowView drv = GeneratedSchedules.Items.GetItemAt(GeneratedSchedules.SelectedIndex) as DataRowView;
-            DateTime date = Convert.ToDateTime(drv["Date"]);
-            ScheduleController sc = new ScheduleController();
-            //Get the correct date of the schedule to be deleted
-            Schedule s = sc.getScheduleByDate(date);
-            //If there is no schedule on that date, throw an error
-            if (s.Id == null)
+            //If there are multiple schedules selected, open up all of them
+            foreach (DataRowView item in GeneratedSchedules.SelectedItems)
             {
-                System.Windows.MessageBox.Show("No Schedule Generated", "Error");
+                DataRowView drv = GeneratedSchedules.Items.GetItemAt(GeneratedSchedules.SelectedIndex) as DataRowView;
+                DateTime date = Convert.ToDateTime(drv["Date"]);
+                ScheduleController sc = new ScheduleController();
+                //Get the correct date of the schedule to be deleted
+                Schedule s = sc.getScheduleByDate(date);
+                //If there is no schedule on that date, throw an error
+                if (s.Id == null)
+                {
+                    System.Windows.MessageBox.Show("No Schedule Generated", "Error");
+                }
+                ScheduleWindow ss = new ScheduleWindow(date);
+                ss.Show();
             }
-           ScheduleWindow ss = new ScheduleWindow(date);
-           ss.Show();
 
         }
 
